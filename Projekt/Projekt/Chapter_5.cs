a625223db280dcc5f75769ad53e9d772d166eaa6
AsosChoroszcz <adamsosnowski56@gmail.com>
2022-06-30 21:24:30 +0200

    zmiany w introduction i chapter5

--- Projekt/Projekt/Chapter_5.cs
+++ Projekt/Projekt/Chapter_5.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.Design;
+using Microsoft.VisualBasic;
 
 namespace Projekt
 {
@@ -8,17 +9,33 @@ namespace Projekt
         public static void Sesja()
         {
             Program.Print("Budzisz się następnego dnia rano.");
-            Program.Print("To jest ten dzień którego tak sie obawiasz.");
-            Program.Print("Budzisz sie rano ");
-            Wyniki();
-            
-            
-            
-            
-            
+            Program.Print("To jest ten dzień którego tak sie obawiasz:");
+            Console.ReadKey();
+            Console.Clear();
+            string title = @"   _____          _       _ _ _ 
+  / ____|        (_)     | | | |
+ | (___   ___ ___ _  __ _| | | |
+  \___ \ / _ | __| |/ _` | | | |
+  ____) |  __|__ \ | (_| |_|_|_|
+ |_____/ \___|___/ |\__,_(_|_|_)
+                _/ |            
+               |__/          ";
+            Console.WriteLine(title);
+            Console.ReadKey();
+            Console.Clear();
+            Program.Print("No nic, trzeba być dobrej myśli");
+            Program.Print("Nie ma co tracić czasu. Stajesz szybko na nogi i przygotowujesz się na dzień próby.");
+            Program.Print("Wychodzisz z domu i wyruszasz zmierzyć się ze swoim przeznaczeniem.");
             Console.WriteLine("Wciśnij dowolny przycisk, aby kontynuować...");
             Console.ReadKey();
             Wyniki();
+            
+            
+            
+            
+            
+            
+            
             
         }
         static private Random rand = new Random();
@@ -26,14 +43,17 @@ namespace Projekt
         {
             Random rand = new Random();
             int r = rand.Next(0, 101);
-            if (Program.postac.szansa<=r)
+            if (Program.postac.szansa>=r)
             {
-                Program.Print("No i git, udało sie");
+                
+                Program.Print("Gratulacje! Udało ci się zdać semestr (Dobre zakończenie)");
             }
             else
             {
-                Program.Print("Niestety nie udało się. Trzeba będzie dociskać na drugi termin");
+                Program.Print("Niestety nie udało ci się. Trzeba będzie dociskać na drugi termin. (Złe zakończenie)");
             }
+            Console.WriteLine(Program.postac.szansa);
+            Console.WriteLine(r);
         }
     }
 }
